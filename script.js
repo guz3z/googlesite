b2924432a0117ed6a2f859c987d2d3a7bddf0113
hxckey <george.h@live.com>
2021-09-29 10:12:01 +0100

    change html

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-const input = document.getElementById('myInputContainer')
+const input = document.getElementById('searchbar')
 const photoSpace = document.getElementById('photo-space')
+// import cors from 'cors';
 
 input.addEventListener('keydown', function(event){
     if(event.key === 'Enter')
         loadImg();
 })
 
-function loadImg(){
-    removeImages();
-    const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=1&client_id=taligljG-L0DlFOiWbVJD2JLhuVq1dKlmMclzWyKtU8'
+function loadImg(response, request){
+    // removeImages();
+    const url = `https://api.unsplash.com/search/photos/?query=${input.value}&per_page=10&client_id=taligljG-L0DlFOiWbVJD2JLhuVq1dKlmMclzWyKtU8`
     fetch(url)
-    .then(response => {
-        console.log(response);
-            if(response.ok)
-                return response.json()
-            else
-                alert(response.status)
-            })
-    .then(data =>{
-        const imageNodes = [];
-        for(let i = 0; i < data.results.length; i++){
-            imageNodes[i] = document.createElement('div');
-            imageNodes[i].className = 'img';
-            imageNodes[i].style.backgroundImage = 'url('+data.results[i].urls.raw+')';
-        }
-    })
+    .then(response => response.json())
+    // let data = response.json()
+    // {
+        // console.log(response);
+        //     if(response.ok)
+        //         return response.json.stringify
+        //     else
+        //         alert(response.status)
+        //     })
+    .then(data => generateContent(data));
+    console.log(response)
+    // console.log(data);
+        // {
+        // const imageNodes = [];
+        // for(let i = 0; i < data.results.length; i++){
+        //     imageNodes[i] = document.createElement('div');
+        //     imageNodes[i].className = 'img';
+        //     imageNodes[i].style.backgroundImage = 'url('+data.results[i].urls.raw+')';
+        // }
+    // }
+    // )
 }
 
 function removeImages(){
